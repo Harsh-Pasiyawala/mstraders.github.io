5099433e7eab02912c071076e83e50d2ef215468
Harsh-Pasiyawala <zarims03@gmail.com>
2021-07-20 20:56:40 +0530

    Added new content

---
+++ PRODUCTPAGES/KORA/One Pel Kora/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $('.color-choose img').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+
+});
